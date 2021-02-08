a083c9d40c10255f9c87300e343cb3b3f8b9ecb4
Dmitry Mikhaylov <dmikhaylov7@gmail.com>
2021-02-08 11:17:25 +0300

    updating adaptive 2p2 block

--- src/components/P2PComponent/index.js
+++ src/components/P2PComponent/index.js
@@ -18,6 +18,7 @@ export default class P2PComponent extends React.Component {
                     <div className="row">
                         <div className="col-md-12">
                             <h3 className="extraBold">Преимущества для заемщика</h3>
+                            <img src={p2p_1Img} className="img1 img1_mobile" draggable={false} alt="" />
                             <h4 className="text1">Сохраняйте BTC</h4>
                             <h5 className="text1">
                                 Не продавайте биткоин, чтобы получить деньги.
@@ -27,46 +28,52 @@ export default class P2PComponent extends React.Component {
                             </h5>
                             <img src={p2p_1Img} className="img1" draggable={false} alt="" />
                             <div className="cardsWrapper wrapper1">
-                                <ScrollAnimation
-                                    delay={150}
-                                    animateOnce={true}
-                                    animateIn="fadeInUp">
-                                    <div className="greenCard card1">
-                                        <div className="greenCard_title bold">
-                                            Без KYC
+                                <div className="card_wrapp">
+                                    <ScrollAnimation
+                                        delay={150}
+                                        animateOnce={true}
+                                        animateIn="fadeInUp">
+                                        <div className="greenCard card1">
+                                            <div className="greenCard_title bold">
+                                                Без KYC
+                                            </div>
                                         </div>
-                                    </div>
-                                </ScrollAnimation>
-                                <ScrollAnimation
-                                    delay={0}
-                                    animateOnce={true}
-                                    animateIn="fadeInUp">
-                                    <div className="blackCard card1">
-                                        <div className="blackCard_title bold">
-                                            Без кредитной<br />истории
+                                    </ScrollAnimation>
+                                </div>
+                                <div className="card_wrapp">
+                                    <ScrollAnimation
+                                        delay={0}
+                                        animateOnce={true}
+                                        animateIn="fadeInUp">
+                                        <div className="blackCard card1">
+                                            <div className="blackCard_title bold">
+                                                Без кредитной<br />истории
+                                            </div>
+                                            <div className="blackCard_text">
+                                                Не нужно предоставлять сведения<br />
+                                                о кредитной истории и доказывать<br />
+                                                свою платежеспособность.
+                                            </div>
                                         </div>
-                                        <div className="blackCard_text">
-                                            Не нужно предоставлять сведения<br />
-                                            о кредитной истории и доказывать<br />
-                                            свою платежеспособность.
+                                    </ScrollAnimation>
+                                </div>
+                                <div className="card_wrapp">
+                                    <ScrollAnimation
+                                        delay={300}
+                                        animateOnce={true}
+                                        animateIn="fadeInUp">
+                                        <div className="grayCard">
+                                            <div className="grayCard_title bold">
+                                                Не переплачивайте
+                                            </div>
+                                            <div className="grayCard_text">
+                                                Возьмите кредит в USDT на срок
+                                                от 7 дней до 1 года.<br />
+                                                Погасите кредит досрочно и платите только за срок пользования деньгами.
+                                            </div>
                                         </div>
-                                    </div>
-                                </ScrollAnimation>
-                                <ScrollAnimation
-                                    delay={300}
-                                    animateOnce={true}
-                                    animateIn="fadeInUp">
-                                    <div className="grayCard">
-                                        <div className="grayCard_title bold">
-                                            Не переплачивайте
-                                        </div>
-                                        <div className="grayCard_text">
-                                            Возьмите кредит в USDT на срок
-                                            от 7 дней до 1 года.<br />
-                                            Погасите кредит досрочно и платите только за срок пользования деньгами.
-                                        </div>
-                                    </div>
-                                </ScrollAnimation>
+                                    </ScrollAnimation>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -74,6 +81,7 @@ export default class P2PComponent extends React.Component {
                         <div className="col-md-7">
                             <img src={p2p_2Img} className="img2" draggable={false} alt="" />
                             <h3 className="extraBold text2">Преимущества для кредитора</h3>
+                            <img src={p2p_2Img} className="img2 img2_mobile" draggable={false} alt="" />
                             <h5 className="text2">
                                 Возможность заработать криптоактив не рискуя своим капиталом.
                                 Если заемщик не закрыл кредит- вы получаете залоговую валюту (BTC).
